c3c87aeae63a3dfe9d3f8e1b772cd5b6c5c9b462
bigboy32 <34939884+bigboy32@users.noreply.github.com>
2019-09-25 16:24:57 +0200

    Adding More functions
    In this Commit i modified the code to do it only once so to run it only once!
    
    (I mean you cann run it twice but it will not loop forever!)

--- MacosMojaveChangeTheme.py
+++ MacosMojaveChangeTheme.py
@@ -1,66 +1,65 @@
-import os
-import datetime
+# Made by Github/BigBoy32
+'''
+Made for Everyone!
 
+Please Change the Code: * Link your files (CTRL-H find all my files and replace with yours), Set Your timeings!
 
-ThemeList = ['Mc-OS-CTLina-Dark','Mojave-light-alt']
+If you wan't Download the Wallpapers and the Themes (Move the themes to .themes or make .themes and move them)
 
-Nights = ['/home/anantha/Pictures/Wallpapers/Night.jpg','/home/anantha/Pictures/Wallpapers/Middnight.jpg']
-Morrnings = ['/home/anantha/Pictures/Wallpapers/Morrning.jpg','/home/anantha/Pictures/Wallpapers/Noon.jpg','/home/anantha/Pictures/Wallpapers/Evening.jpg']
+Run the code when you wan't to check the time and automaticly Change it!
 
-while True:
+Feel free to edit and comment issues!
+'''
 
-    CurrTime = datetime.datetime.now()
+CurrTime = datetime.datetime.now()
 
-    Hour = CurrTime.hour
+Hour = CurrTime.hour
 
-     # print(Hour)
+TimeList = ['06','12','17','20','24']
 
-    TimeList = ['06','12','17','19','24']
+Morrning = TimeList[0]
 
-    Morrning = TimeList[0]
+Morrning = int(Morrning)
 
-    Morrning = int(Morrning)
+Noon = TimeList[1]
 
-    Noon = TimeList[1]
+Noon = int(Noon)
 
-    Noon = int(Noon)
+Evening = TimeList[2]
 
-    Evening = TimeList[2]
+Evening = int(Evening)
 
-    Evening = int(Evening)
+Night = TimeList[3]
 
-    Night = TimeList[3]
+Night = int(Night)
 
-    Night = int(Night)
+Middnight = TimeList[4]
 
-    Middnight = TimeList[4]
+Middnight = int(Middnight)
 
-    Middnight = int(Middnight)
+Hour = int(Hour)
 
-    Hour = int(Hour)
+if Hour >= Morrning and Noon > Hour:
 
-    if Hour >= Morrning and Noon > Hour:
+    os.system("gsettings set org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Morrning.jpg'")
 
-        os.system("gsettings set org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Morrning.jpg'")
+    os.system('gsettings set org.gnome.desktop.interface gtk-theme "Mojave-light-alt"')
+elif Hour >= Noon and Evening > Hour:
 
-        os.system('gsettings set org.gnome.desktop.interface gtk-theme "Mojave-light-alt"')
-    elif Hour >= Noon and Evening > Hour:
+    os.system("gsettings set org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Noon.jpg'")
 
-        os.system("gsettings get org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Noon.jpg'").format(Morrnings[1])
+    os.system('gsettings set org.gnome.desktop.interface gtk-theme "Mojave-light-alt"')
+if Hour >= Evening and Night > Hour:
 
-        os.system('gsettings set org.gnome.desktop.interface gtk-theme "{}"'.format(ThemeList[1]))
-    if Hour >= Evening and Night > Hour:
+    os.system("gsettings set org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Evening.jpg'")
 
-        os.system("gsettings get org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Evening.jpg'").format(Morrnings[2])
+    os.system('gsettings set org.gnome.desktop.interface gtk-theme "Mc-OS-CTLina-Dark"')
+if Hour >= Night and Middnight > Hour:
 
-        os.system('gsettings set org.gnome.desktop.interface gtk-theme "{}"'.format(ThemeList[1]))
-    if Hour >= Night and Middnight > Hour:
+    os.system("gsettings set org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Night.jpg'")
 
-        os.system("gsettings get org.gnome.desktop.background picture-uri 'file:///{}'").format(Nights[0])
+    os.system('gsettings set org.gnome.desktop.interface gtk-theme "Mc-OS-CTLina-Dark"')
+if Hour == Middnight:
+    os.system("gsettings set org.gnome.desktop.background picture-uri 'file:////home/anantha/Pictures/Wallpapers/Middnight.jpg'")
 
-        os.system('gsettings set org.gnome.desktop.interface gtk-theme "{}"'.format(ThemeList[0]))
-    if Hour == Middnight:
-
-        os.system("gsettings get org.gnome.desktop.background picture-uri 'file:///{}'").format(Nights[1])
-
-        os.system('gsettings set org.gnome.desktop.interface gtk-theme "{}"'.format(ThemeList[0]))
+    os.system('gsettings set org.gnome.desktop.interface gtk-theme "Mc-OS-CTLina-Dark"')
